57610cf389513b23bc46438ae810cd3b185f136a
HA <ha@hataeseon-ui-MacBookAir.local>
2023-01-07 14:21:20 +0900

    feat: 프로젝트 환경설정

---
+++ PointAndLineToPlane/PointAndLineToPlane/ViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PointAndLineToPlane
+//
+//  Created by HA on 2023/01/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
